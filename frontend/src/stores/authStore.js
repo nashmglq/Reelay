12ed8c0ca2d1106a87cfea4a7d0335ddd86f606e
Nash Maglaqui <nashmglq89@gmail.com>
2025-06-04 20:22:18 +0800

    Sick push

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -43,3 +43,44 @@ export const authStore = create(devtools((set) => ({
     }
   },
 })));
+
+
+// export const newChat = create(devtools((set) => ({
+//   loading: false,
+//   success: false,
+//   error: null,
+//   message: [],
+
+//   login: async (credentials) => {
+//     try {
+//       set({ loading: true, success: false, error: false, message: [] });
+//    console.log(baseUrl)
+//       const response = await axios.post(
+//         `${baseUrl}/auth/google-auth`,
+//         credentials
+//       );
+   
+//       console.log(response)
+//       if (response.data && response.data.success) {
+//         localStorage.setItem("userInfo", JSON.stringify(response.data.success));
+//         set({
+//           loading: false,
+//           success: true,
+//           error: false,
+//           message: response.data.success,
+//         });
+//       }
+//     } catch (err) {
+//       set({
+//         loading: false,
+//         success: false,
+//         error: true,
+//         message:
+//           err.response && err.response.data.error
+//             ? err.response.data.error
+//             : "Something went wrong",
+//       });
+//     }
+//   },
+// })));
+
