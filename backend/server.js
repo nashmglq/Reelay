f6498e70733418d6cb9b07dcf258ada521608aad
Nash Maglaqui <nashmglq89@gmail.com>
2025-07-20 00:04:08 +0800

    Fixed: Fixed the limit to 10mb

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const genAiRoute = require("./routes/genAiRoute")
 // Static to get all locally such as the image
 app.use('/uploads', express.static(path.join(__dirname, "uploads")));
 app.use("/genImage", express.static(path.join(__dirname, "genImage")));
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoute)
