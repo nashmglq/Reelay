6dc7404935854351ac30878b508e71ea2332fae2
Nash Maglaqui <nashmglq89@gmail.com>
2025-07-29 19:58:36 +0800

    feat: Store Pay

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -466,3 +466,54 @@ export const deleteChatStore = create(
     },
   }))
 );
+
+
+export const paymentStore = create(
+  devtools((set) => ({
+    loading: false,
+    success: false,
+    error: false,
+    message: [],
+
+    payment: async (id) => {
+      try {
+        set({ loading: true, success: false, error: false, message: [] });
+
+        const getToken = JSON.parse(localStorage.getItem("userInfo"));
+        const token = getToken ? getToken.token : null;
+        const config = token
+          ? {
+              headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          : null;
+
+        const response = await axios.get(
+          `${baseUrl}/crud-genAi/get-detail-chat/${id}`,
+          config
+        );
+
+        if (response.data && response.data.success) {
+          set({
+            loading: false,
+            success: true,
+            error: false,
+            message: response.data.success,
+          });
+        }
+      } catch (err) {
+        set({
+          loading: false,
+          success: false,
+          error: true,
+          message:
+            err.response && err.response.data
+              ? err.response.data.error
+              : "Something went wrong.",
+        });
+      }
+    },
+  }))
+);
