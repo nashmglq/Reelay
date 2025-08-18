b8719db8145ea0ebd17a2eb9e4af69e00e395a78
Nash Maglaqui <nashmglq89@gmail.com>
2025-08-18 21:35:16 +0800

    Feat: Router for FindUser

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { contactAdmin, getUsers } = require("../controller/admin");
+const { contactAdmin, getUsers, findUser } = require("../controller/admin");
 const adminRoute = express.Router();
 const { authCheck } = require("../middleware/middleware");
 
 adminRoute.post("/contact-us", contactAdmin)
 adminRoute.get("/get-users", authCheck, getUsers)
+adminRoute.post("/get-user-query", authCheck, findUser )
 
 
 module.exports = {
